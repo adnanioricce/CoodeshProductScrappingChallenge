c5bf3a9d1be5c9f4113f96ef6b2eeb6e1bff0fff
adnanioricce <adnanioricce@outlook.com>
2022-12-29 10:03:15 -0300

    [Scrapper] - Writing the first version of the scrapper

---
+++ Scrapper/ScrapperWorker.cs
@@ -0,0 +1,29 @@
+﻿using ProductScrapper;
+namespace Scrapper
+{
+    internal sealed class ScrapperWorker : BackgroundService
+    {
+        private readonly ProductScrapper.ProductScrapper _scrapper;
+        private readonly IProductRepository _productRepository;
+        private readonly string _initialUrl = "https://world.openfoodfacts.org/";
+        public ScrapperWorker(ProductScrapper.ProductScrapper scrapper,IProductRepository productRepository)
+        {
+            _scrapper = scrapper;
+            _productRepository = productRepository;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var client = new HttpClient();
+            var products = await _scrapper.ScrapProductListAsync(client.GetStringAsync, _initialUrl) ?? Enumerable.Empty<ProductDto>();
+            //TODO: Save products
+            //await _productRepository.BulkCreateAsync(products);
+            //TODO: Scrap remaining data for each products
+            var productsFilled = new ProductDto[products.Count()];
+            foreach(var (Index,Value) in products.Select((p,index) => (index,p) ))
+            {
+                productsFilled[Index] = await _scrapper.ScrapProductDescription(client.GetStringAsync, _initialUrl,Value);                
+            }
+            await _productRepository.BulkCreateAsync(products);
+        }
+    }
+}
