789b77a75b8cb6e79ca66f93bc136a66c1acf8ef
adnanioricce <adnanioricce@outlook.com>
2022-12-29 10:05:46 -0300

    [Testing] - Writing more tests for api,parser and scrapper
    to avoid duplication, a file with the strings of the pages to be
    scrapped was created.

--- UnitTests/ApiTests.fs
+++ UnitTests/ApiTests.fs
@@ -1,20 +1,77 @@
 ﻿namespace Api
+
+open System.Collections.Generic
+
 module Tests = 
 
     open System
     open Xunit
     open ProductScrapper
     open Microsoft.AspNetCore.Http
+    open Microsoft.AspNetCore.Http.HttpResults
     //Parser tests
     //TODO: Unit tests for NoContent,Server Error
     [<Fact>]
     let ``GET list product returns ok if not empty`` () = async {    
         let repository:IProductRepository = FakeProductRepository()
         let! products = repository.ListAsync() |> Async.AwaitTask
-        let func () = ProductEndpoints.ListAsync repository
-        let! r = func () |> Async.AwaitTask
-        let expectedResult = Results.Ok(products)
-        Assert.Equal(expectedResult,r)
+        let listProductsAsync = repository.ListAsync
+        let func () = ProductEndpoints.ListAsync listProductsAsync
+        let! r = (func () |> Async.AwaitTask)
+        let result = r :?> HttpResults.Ok<IEnumerable<ProductDto>>
+        let expectedResult = Results.Ok(products) :?> HttpResults.Ok<Object>                
+        let receivedProducts = result.Value
+        Assert.Equal(expectedResult.StatusCode,result.StatusCode)
+        Assert.Equal<IEnumerable<ProductDto>>(products,receivedProducts)
     }
+    [<Fact>]
+    let ``GET list product returns no content empty`` () = async {            
+        let listProductsFunc = 
+            fun _ -> 
+                task{ 
+                    let products:IEnumerable<ProductDto> = []
+                    return products
+                } 
+        let listProductsAsync:ListProductsAsync = listProductsFunc 
+        let func () = ProductEndpoints.ListAsync listProductsAsync
+        let! r = (func () |> Async.AwaitTask)    
+        let result = r :?> HttpResults.NoContent
+        let expectedResult = Results.NoContent() :?> HttpResults.NoContent
+        let isEqual = r = expectedResult        
+        Assert.Equal(expectedResult.StatusCode,result.StatusCode);
+    }
+    [<Fact>]
+    let ``GET list product returns server error if a unexpected error ocurred`` () = async {            
+        let listProductsFunc = 
+            fun _ -> 
+                task{ 
+                    let products:IEnumerable<ProductDto> = []
+                    Exception("Unexpected error") |> raise
+                    return products
+                }
+        let listProductsAsync:ListProductsAsync = listProductsFunc
+        let func () = ProductEndpoints.ListAsync listProductsAsync
+        let! r = (func () |> Async.AwaitTask)
+        let result = r :?> HttpResults.StatusCodeHttpResult
+        let expectedResult = Results.StatusCode(500) :?> HttpResults.StatusCodeHttpResult        
+        Assert.Equal(expectedResult.StatusCode,result.StatusCode)
+    }    
     //TODO: Unit tests for the products/:code
+    [<Fact>]
+    let ``GET product by code expect to return a product if id exists`` () = async {
+        let repository:IProductRepository = FakeProductRepository()
+        let! products = repository.ListAsync() |> Async.AwaitTask
+        let product = products |> Seq.head
+        let mockGetProductByCode = 
+            fun (code:int64)-> 
+                task{                     
+                    return product
+                }        
+        //let mockFunc = GetByCodeAsync(mockGetProductByCode)
+        let funcUnderTest code = ProductEndpoints.GetByCodeAsync(mockGetProductByCode,code)
+        let! r = (funcUnderTest 1 |> Async.AwaitTask)
+        let result = r :?> HttpResults.Ok<ProductDto>
+        let expectedResult = Results.Ok<ProductDto>(product) :?> HttpResults.Ok<ProductDto>        
+        Assert.Equal(expectedResult.StatusCode,result.StatusCode)
+    }
     
---
+++ UnitTests/ScrapperTests.fs
@@ -0,0 +1,32 @@
+﻿namespace Scrapper
+
+open System.Collections.Generic
+
+module Tests = 
+
+    open System
+    open Xunit
+    open ProductScrapper
+    //TODO:
+
+    [<Fact>]
+    let ``scrapper receives the url and scrap all products of the page`` () = async {
+        let url = "https://world.openfoodfacts.org/"
+        let mockGetString = fun (url:string) -> task { return Tests.Data.allProductsDivStr }
+        let scrapper = ProductScrapper()
+        let! products = scrapper.ScrapProductListAsync(mockGetString,url) |> Async.AwaitTask
+        Assert.NotEmpty(products)
+    }
+    //Note: the products scrapped from the initial page have the link to the description page
+    [<Fact>]
+    let ``complete product must be retrieved from individual description page for each product`` () = async {
+        let url = "https://world.openfoodfacts.org/product/3017620422003/nutella-ferrero"
+        let mockGetString = fun (url:string) -> task { return Tests.Data.singleProductStr }
+        let scrapper = ProductScrapper()        
+        let! product = scrapper.ScrapProductDescription(mockGetString,url,ProductDto()) |> Async.AwaitTask
+        Assert.NotEmpty(product.Barcode)
+        Assert.NotEmpty(product.Brands)
+        Assert.NotEmpty(product.Categories)
+        Assert.NotEmpty(product.Quantity)
+        Assert.NotEmpty(product.Packaging)
+    }
